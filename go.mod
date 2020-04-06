6628b16d2d141cd7437c634f5e7b7a671c39b656
William Wesley Monroe <ww.monroe@gmail.com>
2020-04-06 07:34:32 -0500

    Square one: using the go test framework

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/purplehey/junk
 
 go 1.14
-
-// require rsc.io/quote v1.5.2
--- math_test.go
+++ math_test.go
@@ -2,12 +2,10 @@ package math
 
 import (
 	"testing"
-
-	"/github.com/purple/junk"
 )
 
-func TestSum(t *testing.T)  {
-	sum := math.Sum([]int{5, 8, -3})
+func TestSum(t *testing.T) {
+	sum := Sum([]int{5, 8, -3})
 
 	if sum != 10 {
 		t.Errorf("Expected 10, got %d", sum)
